68add7845843c55602a7f19516339a3537daf5a5
Rostyslav Reznichenko <RostikRezn@gmail.com>
2023-09-10 21:24:59 +0300

    Change base path for secrets

--- vault-init.sh
+++ vault-init.sh
@@ -1,4 +1,4 @@
 export VAULT_TOKEN="00000000-0000-0000-0000-000000000000"
 export VAULT_ADDR="http://127.0.0.1:8200"
-vault kv put secret/spring-mvc spring.kafka.bootstrap-servers=broker:29092 spring.application.name=spring-mvc-vault truststore.path=file:/services/vault/certs.jks truststore.password=12345
+vault kv put secret/service-internal/spring-mvc spring.kafka.bootstrap-servers=broker:29092 spring.application.name=spring-mvc-vault truststore.path=file:/services/vault/certs.jks truststore.password=12345
 #vault kv put secret/spring-mvc spring.kafka.bootstrap-servers=broker:29092 spring.application.name=spring-mvc-vault
